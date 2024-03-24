af2cda09eff88e0413140ff0ff61e953004cccf8
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-03-24 15:53:17 +0100

    chor: update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@
 
 helm repo add argo https://argoproj.github.io/argo-helm
 helm install argocd argo/argo-cd --namespace argocd --create-namespace --dependency-update --set argo-cd.configs.secret.argocdServerAdminPassword=$ARGOCD_ADMIN_PWD
-helm upgrade argocd ./bootstrap/argo/ --namespace argocd -f ./bootstrap/argo/values.yaml
+helm upgrade argocd ./bootstrap/argo/ --namespace argocd  --dependency-update -f ./bootstrap/argo/values.yaml --set argo-cd.configs.secret.argocdServerAdminPassword=$ARGOCD_ADMIN_PWD
